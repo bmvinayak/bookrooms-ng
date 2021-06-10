66cd9743c5cf69a4a24f88a7399de5e506443d80
Vinayak Mungurwadi <bmvinayak@yahoo.com>
2021-06-10 23:19:29 +0530

    Deployment to Heroku

--- server/index.js
+++ server/index.js
@@ -17,22 +17,16 @@ require('./model/rentalItems');
 require('./model/bookings');
 require('./model/cloudinaryImage');
 
-dotenv.config();
-myMongoose.connect(process.env.DB_URI, { 
-	useNewUrlParser: true, 
-	useCreateIndex: true, 
+console.log(path.resolve(process.cwd()));
+dotenv.config({ path: path.resolve(process.cwd(), 'server','.env')});
+myMongoose.connect(process.env.DB_URI, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
 	useUnifiedTopology: true
 }, () => {
 	console.log('Connected to  DB')
 });
-/*
-.then (() => {
-		if (process.env.NODE_ENV !== 'production')	{
-			//const generateDb = new GenerateDb();
-			//generateDb.seedDb();
-		}
-	});
-*/
+
 const app = myExpress();
 const PORT = process.env.PORT || 3001;
 
@@ -42,21 +36,21 @@ app.use(provideErrorHandler);
 app.use('/api/v1/rentals', RentalRouter);
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/bookings', bookingRouter);
-app.use('/api/v1/image',imageRouter);
+app.use('/api/v1/image', imageRouter);
 
 if (process.env.NODE_ENV === 'production') {
-	const appPath = path.join(__dirname, '..', 'dist/bwm-ng');
+	const appPath = path.join(__dirname, '..', 'dist');
 	app.use(myExpress.static(appPath));
 
 	app.get('*', function (req, res) {
-		res.sendFile(path.resolve(appPath, 'index.html'));
+		return res.sendFile(path.resolve(appPath, 'index.html'));
 	})
 }
 
 
 
-app.listen(PORT, function() {
-	console.log('I am listening at Port :'+PORT);
+app.listen(PORT, function () {
+	console.log('I am listening at Port :' + PORT);
 	console.log('And the environment is :' + process.env.NODE_ENV);
 });
 
